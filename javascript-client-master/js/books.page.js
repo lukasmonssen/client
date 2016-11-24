da9bb9a464cf18350e9cadf391d650ce86afb791
lukasmonssen <lukasmonssen@hotmail.com>
2016-11-24 12:45:55 +0100

    Add files via upload
    første commit

---
+++ javascript-client-master/js/books.page.js
@@ -0,0 +1,30 @@
+function getBooks ()
+{
+    $.ajax(
+        {
+            url:"https://localhost:8000/getbooks",
+            method: "GET",
+            dataType:"json",
+
+            success: function(data)
+                {
+                    $("#tblBooks").dataTable(
+                        {
+                            data: data,
+                            processing: true,
+                            bDestroy: true,
+                            columns: [
+                                { data: "title" },
+                                { data: "edition" },
+                                { data: "author" },
+                                { data: "isbn" },
+                            ]
+                        });
+                },
+                error: function (data)
+                {
+                    alert(JSON.stringify(data));
+                    alert("Hov! Der opstod en fejl!");
+                }
+        })
+}
